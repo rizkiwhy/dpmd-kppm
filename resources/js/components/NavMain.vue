80961a94b8d193d0afd0c45bf2efba0854c1ed7b
rizkiwhy <rizkiiiwahyudi@gmail.com>
2025-06-12 05:27:15 +0700

    fix(navmain): lint

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem, NavGroup } from '@/types';
+import { NavGroup } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
 
 defineProps<{
@@ -11,7 +11,7 @@ const page = usePage();
 </script>
 
 <template>
-    <SidebarGroup class="px-2 py-0" v-for="item in items">
+    <SidebarGroup class="px-2 py-0" v-for="item in items" :key="item.title">
         <SidebarGroupLabel>{{ item.title }}</SidebarGroupLabel>
         <SidebarMenu>
             <SidebarMenuItem v-for="navItem in item.items" :key="navItem.title">
